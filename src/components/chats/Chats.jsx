8c9243a47aa5181a53d0e31319f9f03a0acf392b
believelody <believelody@gmail.com>
2019-08-10 12:10:57 +0200

    Set default render for chat list

--- src/components/chats/Chats.jsx
+++ src/components/chats/Chats.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { useAppHooks } from "../../contexts";
+import api from "../../api";
 
 const ChatListStyle = styled.div``;
 
 const Chats = () => {
-  return <ChatListStyle>Chats</ChatListStyle>;
+  const {useAuth} = useAppHooks()
+  const [{user}, _] = useAuth
+
+  const [chats, setChats] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const getChats = async () => {
+      if (user) {
+        let c = await api.user.getChatList(user.id)
+        if (c.length > 0) setChats(c)
+      }
+      setLoading(false)
+    }
+
+    getChats()
+  }, [user])
+
+  return (
+    <ChatListStyle>
+      {
+        !loading && chats.length === 0 && <h4>You have no chats. Choose a friend and start talking.</h4>
+      }
+      {
+        !loading && chats.length > 0 && console.log(chats)
+      }
+    </ChatListStyle>
+  );
 };
 
 export default Chats;
