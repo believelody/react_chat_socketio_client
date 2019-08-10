f4ee208be1d3d3266fd982bc465d56106a999173
believelody <believelody@gmail.com>
2019-08-11 00:36:51 +0200

    Change variables environment

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import chat from "./chat";
 import user from "./user";
 
-export const baseUrl = process.env.REACT_BASE_URL || `http://localhost:5000/api`;
+export const baseUrl = process.env.REACT_APP_BASE_URL || `http://localhost:5000/api`;
 
 export default { chat, user };
--- src/utils/storeToken.js
+++ src/utils/storeToken.js
@@ -1,5 +1,4 @@
 export default res => {
     const { token } = res.data
-    console.log(token)
     localStorage.setItem('chat_token', token)
 }
