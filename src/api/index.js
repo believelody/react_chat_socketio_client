23837098bbfccc1fa1f50241c60d13952d739706
believelody <believelody@gmail.com>
2019-08-11 12:43:52 +0200

    Updated env variables

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import chat from "./chat";
 import user from "./user";
 
-export const baseUrl = process.env.REACT_APP_SERVER_URL || `http://localhost:5000/api`;
+export const baseUrl = process.env.REACT_APP_SERVER_URL;
 
 export default { chat, user };
