2c862f579974c923a603fcbbc143c6195bb48db8
believelody <believelody@gmail.com>
2019-08-11 17:13:17 +0200

    Change server

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,5 @@ import axios from "axios";
 import chat from "./chat";
 import user from "./user";
 
-axios.defaults.baseUrl =
-  process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
+axios.defaults.baseUrl = "http://localhost:5000";
 export default { chat, user };
