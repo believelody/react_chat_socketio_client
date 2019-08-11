f86a31ea9d195b1707f8333d21037af9ae737080
believelody <believelody@gmail.com>
2019-08-11 16:58:04 +0200

    Change server url

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,6 @@ import axios from "axios";
 import chat from "./chat";
 import user from "./user";
 
-axios.defaults.baseUrl = "http://localhost:5000";
-// axios.defaults.headers.common['Content-Type'] = 'application/json';
-// axios.defaults.headers.common['Access-Content-Allow-Origin'] = '*';
-// axios.defaults.headers.common['crossDomain'] = true;
-// axios.defaults.headers.common['Content-Type'] = 'application/json';
-console.log(axios.defaults);
+axios.defaults.baseUrl =
+  process.env.REACT_APP_SERVER_URL || "http://localhost:5000";
 export default { chat, user };
