142fbe7ad1b4903c631502a76ae009adc2ca8cff
believelody <believelody@gmail.com>
2019-08-11 20:02:14 +0200

    Fix broken server url

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import chat from "./chat";
 import user from "./user";
 
 export let baseUrl = process.env.REACT_APP_SERVER_URL
-  ? process.env.REACT_APP_SERVER_URL
+  ? `${process.env.REACT_APP_SERVER_URL}/api`
   : "http://localhost:5000/api";
 export default { chat, user };
