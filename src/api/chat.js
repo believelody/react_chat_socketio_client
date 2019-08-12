cec06f485b063ed1a229d191ae57befa1c515063
believelody <believelody@gmail.com>
2019-08-12 14:44:31 +0200

    Update api url structure

--- src/api/chat.js
+++ src/api/chat.js
@@ -4,20 +4,20 @@ import { baseUrl } from ".";
 const defaultPath = "chats";
 
 export default {
-  getChats: () => axios.get(`/${baseUrl}/${defaultPath}`),
+  getChats: () => axios.get(`${baseUrl}/${defaultPath}`),
 
   searchChatByUsers: users =>
-    axios.get(`/${baseUrl}/${defaultPath}/searching-chat?users=${users}`),
+    axios.get(`${baseUrl}/${defaultPath}/searching-chat?users=${users}`),
 
-  getChat: id => axios.get(`/${baseUrl}/${defaultPath}/${id}`),
+  getChat: id => axios.get(`${baseUrl}/${defaultPath}/${id}`),
 
-  createChat: data => axios.post(`/${baseUrl}/${defaultPath}`, data),
+  createChat: data => axios.post(`${baseUrl}/${defaultPath}`, data),
 
   addUserToChat: (id, user) =>
-    axios.post(`/${baseUrl}/${defaultPath}/${id}`, user),
+    axios.post(`${baseUrl}/${defaultPath}/${id}`, user),
 
   removeUserToChat: (id, userId) =>
-    axios.delete(`/${baseUrl}/${defaultPath}/${id}`, userId),
+    axios.delete(`${baseUrl}/${defaultPath}/${id}`, userId),
 
-  deleteChat: id => axios.delete(`/${baseUrl}/${defaultPath}/${id}`)
+  deleteChat: id => axios.delete(`${baseUrl}/${defaultPath}/${id}`)
 };
