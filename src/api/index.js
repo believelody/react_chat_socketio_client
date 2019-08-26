131bd9cadaaa4c024f1739abb65a009e2ed33400
believelody <believelody@gmail.com>
2019-08-26 20:22:30 +0200

    Debug cors issue try 1

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import chat from "./chat";
 import user from "./user";
 
-export let baseUrl = process.env.REACT_APP_SERVER_URL
+export let baseUrl = !process.env.REACT_APP_SERVER_URL
   ? `${process.env.REACT_APP_SERVER_URL}/api`
   : "http://localhost:5000/api";
 export default { chat, user };
